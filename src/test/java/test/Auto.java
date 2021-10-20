9931ec01c50307540ed8d15f3209a83c277a597e
Dajovih <80296654+Dajovih@users.noreply.github.com>
2021-10-19 20:08:43 -0500

    Cambios

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -10,13 +10,24 @@ public class Auto {
 	
 	int cantidadAsientos() {
 		int resultado=0;
-		for (int i=0; i<asientos.length;i++) {
-			resultado=resultado+1;
+		for (int i = 0; i < asientos.length; i++) {
+			if asientos[i]!=null
+					resultado=resultado+1;
 		}
 		return resultado;
 	}
 	
 	String verficarIntegridad() {
+		String resultado="Auto original"
+		for (int i = 0; i < asientos.length; i++) {
+			if asientos[i]!=null
+					if (motor.registro==registro==asientos[i].registro)
+						continue;
+					else
+						resultado="Las piezas no son originales";
+						break;
+		}
+		return resultado
 		
 	}
 	
